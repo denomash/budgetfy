81990ea27fcaa8c42665f6dcc2a33c0d26f7208c
Dennis Macharia <dennismacharia4747@gmail.com>
2019-10-15 15:41:15 +0300

    set up redux

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import AppRoutes from './routes/AppRroute';
+
+const store = configureStore();
 
 ReactDOM.render(<AppRoutes />, document.getElementById('app'));
