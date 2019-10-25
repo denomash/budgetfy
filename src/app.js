fe7852274154d62d8a1ff1b0a753c5fde4d7af83
Dennis Macharia <dennismacharia4747@gmail.com>
2019-10-25 10:17:47 +0300

    cleanup

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ import { Provider } from 'react-redux';
 
 import AppRoutes from './routes/AppRroute';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
