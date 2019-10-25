d6a429e1dc249da193d329686a175751af49efe3
Dennis Macharia <dennismacharia4747@gmail.com>
2019-10-25 09:38:26 +0300

    cleanup

--- src/app.js
+++ src/app.js
@@ -12,16 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Rent bill', amount: 300 }));
-store.dispatch(
-  addExpense({ description: 'Gas bill', amount: 100, createdAt: 1000 })
-);
-store.dispatch(addExpense({ description: 'Transport bill', amount: 19000 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const app = (
   <Provider store={store}>
     <AppRoutes />
