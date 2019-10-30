07506df9427ec8705a144187f184b465bd319982
Dennis Macharia <dennismacharia4747@gmail.com>
2019-10-30 11:14:19 +0300

    setup test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB642Z-UbD5XrpSUlL3wZsOJ_yKRYqkGJ4',
-  authDomain: 'burgetify.firebaseapp.com',
-  databaseURL: 'https://burgetify.firebaseio.com',
-  projectId: 'burgetify',
-  storageBucket: 'burgetify.appspot.com',
-  messagingSenderId: '875712097180',
-  appId: '1:875712097180:web:d689e68d4bab948860b659',
-  measurementId: 'G-3FMFZR5L6J'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
